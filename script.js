24d0481e43696fce88ed1d79e0fe99338a1d699a
asasri157 <155147688+asasri157@users.noreply.github.com>
2024-08-03 18:27:33 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function updateTime() {
+    const clockElement = document.getElementById('clock');
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateTime, 1000);
+updateTime();
